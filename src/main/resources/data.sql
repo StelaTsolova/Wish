52a3dcb8043e79fe7115b82af4730bd1098309b0
Stela Tsolova <stela.tsolova@gmail.com>
2022-08-13 10:36:26 +0300

    Update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,9 @@
 INSERT INTO CATEGORIES(NAME)
-VALUES ('Shoes'),
-       ('Sunglasses');
+VALUES ('Shoes');
 
 INSERT INTO PRODUCTS(CATEGORY_ID, NAME, PRICE)
-VALUES (1, 'maratonki', 50),
-       (1, 'japanki', 20),
-       (2, 'krygli ochila', 30);
+VALUES (1, 'Sneakers', 50),
+       (1, 'Slippers', 20);
 
 INSERT INTO PICTURES(FOLDER_NAME, PUBLIC_ID, URL, PRODUCT_ID)
 VALUES ('1', '1/01_5904248494766_rz__p27prl',
@@ -23,33 +21,13 @@ VALUES ('1', '1/01_5904248494766_rz__p27prl',
        ('2', '2/0000208693059_02_kt_nvrg1d',
         'https://res.cloudinary.com/dj0dxejrk/image/upload/v1659648579/2/0000208693059_02_kt_nvrg1d.jpg', '2');
 
--- INSERT INTO PRODUCTS_PICTURES(PRODUCT_ID, PICTURES_ID)
--- VALUES (1, 1),
---  (1, 2),
---  (1, 3),
---  (1, 4),
---  (2, 5),
---  (2, 6),
---  (2, 7);
-
--- INSERT INTO PRODUCT_IMG_URLS(PRODUCT_ID, IMG_URLS)
--- VALUES (1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ6QmfP-KBgEba3cwMqWh8iZ7mZijHPcEwoA&usqp=CAU'),
---        (1,
---         'https://maksoft.net/img_preview.php?image_file=web/images/upload/maksoft/web_images.png&img_width=300&ratio=strict'),
---        (1, 'https://снимки.com/wp-content/uploads/2021/09/zalezi1-480x360.jpg'),
---        (2, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ6QmfP-KBgEba3cwMqWh8iZ7mZijHPcEwoA&usqp=CAU'),
---        (3, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQ6QmfP-KBgEba3cwMqWh8iZ7mZijHPcEwoA&usqp=CAU');
-
 INSERT INTO SIZES(NAME)
-VALUES ('XS'),
-       ('S'),
-       ('M'),
-       ('L'),
-       ('XL');
+VALUES ('36'),
+       ('38'),
+       ('40');
 
 INSERT INTO QUANTITIES(AVAILABLE_QUANTITY, PRODUCT_ID, SIZE_ID)
 VALUES (10, 1, 1),
-       (2, 2, 1),
-       (1, 1, 3),
-       (7, 1, 4),
-       (6, 3, 5);
+       (1, 1, 2),
+       (7, 1, 3),
+       (2, 2, 3);
